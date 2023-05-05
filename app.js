ed4772e0a6b21c017fff5e2ce31dcadca0ae6e49
slamensd <58567346+slamensd@users.noreply.github.com>
2023-05-05 14:20:06 +0000

    done

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const CONTRACT_ABI = 'YOUR_CONTRACT_ABI'; // Replace with your contract ABI
 // Connect wallet button event
 document.getElementById('connectWalletButton').addEventListener('click', async () => {
     try {
-        const provider = new wagmi.WalletProvider();
-        const connected = await provider.connect();
-        if (connected) {
-            const signer = provider.getSigner();
-            const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
-            const account = await signer.getAddress();
+        const providerOptions = {}; // Add any specific provider options if needed
+        const web3Modal = new Web3Modal({ providerOptions });
+        const connection = await web3Modal.connect();
+        const provider = new ethers.providers.Web3Provider(connection);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+        const account = await signer.getAddress();
 
-            // Check if the connected wallet is the owner of the contract
-            const contractOwner = await contract.owner();
-            if (account.toLowerCase() === contractOwner.toLowerCase()) {
-                document.getElementById('ownerSection').classList.remove('hidden');
-                document.getElementById('ownerApp').innerHTML = await (await fetch('owner.html')).text();
-            } else {
-                document.getElementById('userSection').classList.remove('hidden');
-                document.getElementById('userApp').innerHTML = await (await fetch('user.html')).text();
-            }
+        // Check if the connected wallet is the owner of the contract
+        const contractOwner = await contract.owner();
+        if (account.toLowerCase() === contractOwner.toLowerCase()) {
+            document.getElementById('ownerSection').classList.remove('hidden');
+            document.getElementById('ownerApp').innerHTML = await (await fetch('owner.html')).text();
+        } else {
+            document.getElementById('userSection').classList.remove('hidden');
+            document.getElementById('userApp').innerHTML = await (await fetch('user.html')).text();
         }
     } catch (error) {
         console.error(error);
